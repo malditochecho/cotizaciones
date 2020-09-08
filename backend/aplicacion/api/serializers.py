cbe22a57026fc8613430177b9f76fed354ad6403
Sergio Rodriguez <sergrodrig@gmail.com>
2020-09-08 02:24:50 -0300

    serializadores de la api cambiadas a HyperlinkedModelSerializer

--- backend/aplicacion/api/serializers.py
+++ backend/aplicacion/api/serializers.py
@@ -2,25 +2,48 @@ from rest_framework import serializers
 from cotizaciones import models
 
 
-class ClienteSerializer(serializers.ModelSerializer):
+class ClienteSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api:cliente-detail')
+
     class Meta:
         model = models.Cliente
-        fields = ('empresa', 'contacto', 'telefonoContacto', 'correoContacto')
+        fields = ('url', 'empresa', 'contacto', 'telefonoContacto', 'correoContacto')
 
 
-class ServicioSerializer(serializers.ModelSerializer):
+class ServicioSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api:servicio-detail')
+
     class Meta:
         model = models.Servicio
-        fields = ('nombre', 'descripcion', 'valorMetroCuadrado',)
+        fields = ('url', 'nombre', 'descripcion', 'valorMetroCuadrado',)
 
 
-class ProductoSerializer(serializers.ModelSerializer):
+class ProductoSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api:producto-detail')
+
     class Meta:
         model = models.Producto
-        fields = ('producto', 'marca', 'fabricante', 'valorUnidad',)
+        fields = ('url', 'producto', 'marca', 'fabricante', 'valorUnidad',)
 
 
 class CotizacionSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api:cotizacion-detail')
+
     class Meta:
         model = models.Cotizacion
-        fields = ('empresa', 'servicio', 'metrosCuadrados', 'producto', 'cantidadProducto',)
+        fields = ('url', 'id', 'empresa', 'servicio', 'metrosCuadrados', 'producto', 'cantidadProducto',)
+        depth = 2
+
+
+"""
+
+Para validar los serializadores
+
+./manage.py shell
+from api.serializers import *
+print(repr(ClienteSerializer()))
+print(repr(ServicioSerializer()))
+print(repr(ProductoSerializer()))
+print(repr(CotizacionSerializer()))
+
+"""
